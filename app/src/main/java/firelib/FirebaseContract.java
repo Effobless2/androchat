0b431d9dc8ed2e1339abc7ff9e953c5d8e223677
romain <romain-l58@hotmail.fr>
2020-01-11 14:59:26 +0100

    create java interface contract

---
+++ app/src/main/java/firelib/FirebaseContract.java
@@ -0,0 +1,21 @@
+package firelib;
+
+public interface FirebaseContract {
+
+        public boolean register();
+
+        public boolean login();
+
+        public String getConverssationById(String id);
+
+        public boolean requestAddition(String id);
+
+        public boolean confirmationAddContact(String id);
+
+        public String createConverssation(String name);
+
+        public boolean sendMessage(String idConv, String msg);
+
+        public boolean deleteConverssation(String idConv);
+
+}
