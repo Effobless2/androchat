0cbc0b783db984d740d79203fff69b5c931ae542
Deboffle Maxime <33808798+Effobless2@users.noreply.github.com>
2020-01-26 21:22:36 +0100

    Initialization of login and logout (#9)

---
+++ app/src/main/java/com/example/androchat/anotherActivityToCreate/TemporaryActivity.java
@@ -0,0 +1,40 @@
+package com.example.androchat.anotherActivityToCreate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.androchat.R;
+import com.example.androchat.authentication.SignInActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class TemporaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temporary);
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(getApplicationContext())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getApplicationContext(), "Signed Out", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/androchat/authentication/SignInActivity.java
@@ -0,0 +1,100 @@
+package com.example.androchat.authentication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.androchat.MainActivity;
+import com.example.androchat.R;
+import com.example.androchat.anotherActivityToCreate.TemporaryActivity;
+import com.example.firelib.managers.UserManagement;
+import com.example.model.User;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                );
+
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(), RC_SIGN_IN);
+            }
+        });
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            Intent intent = new Intent(this, TemporaryActivity.class);
+            startActivity(intent);
+        }
+
+
+/*
+
+
+
+        });*/
+}
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, user.getUid(), Toast.LENGTH_LONG).show();
+                User u = new User();
+                u.setGoogleId(user.getUid());
+                UserManagement.getDocumentRefenceByGoogleId(u)
+                        .continueWith(new Continuation<String, String>() {
+                            @Override
+                            public String then(@NonNull Task<String> task) throws Exception {
+                                String result = task.getResult();
+                                if(result == null){
+                                    ((TextView) findViewById(R.id.textView)).setText("Error");
+                                } else {
+                                    ((TextView) findViewById(R.id.textView)).setText(result);
+                                }
+                                return null;
+                            }
+                        });
+                Intent intent = new Intent(this, TemporaryActivity.class);
+                startActivity(intent);
+                // ...
+            } else {
+
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+}
