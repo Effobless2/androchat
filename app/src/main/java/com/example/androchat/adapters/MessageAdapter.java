5555ab2be88813a37ea55415a3aa7908754ceb4c
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-02-11 01:08:14 +0100

    Message Adapter becomes a RecyclerAdapter for scrolling down bug there is a bug

--- app/src/main/java/com/example/androchat/adapters/MessageAdapter.java
+++ app/src/main/java/com/example/androchat/adapters/MessageAdapter.java
@@ -9,11 +9,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androchat.R;
 import com.example.firelib.managers.UserManagement;
@@ -29,75 +31,115 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class MessageAdapter extends ArrayAdapter<Message> {
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+    List<Message> messages;
+    private LayoutInflater layoutInflater;
     LifecycleOwner activity;
 
-    public MessageAdapter(LifecycleOwner activity, @NonNull Context context, @NonNull List<Message> objects){
-        super(context, 0, objects);
-        this.activity = activity;
+    public MessageAdapter(Context context, LifecycleOwner lifecycleOwner) {
+        layoutInflater = LayoutInflater.from(context);
+        activity = lifecycleOwner;
     }
 
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        final Message message = getItem(position);
-
-        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
-
-        final View messageRow = layoutInflater.inflate(R.layout.message_list_item, null);
-
-        String date;
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-        String today = formatter.format(new Date());
-        String convDate = formatter.format(message.getDate());
-        if (today.compareTo(convDate) == 0){
-            date = new SimpleDateFormat("hh:mm").format(message.getDate());
-        }
-        else {
-            date = convDate;
-        }
-        ((TextView)messageRow.findViewById(R.id.dateTxt)).setText(date);
-
-        ((TextView)messageRow.findViewById(R.id.messageTxt)).setText(message.getContent());
-
-        if(message.getId_user().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
-            ((LinearLayout)messageRow.findViewById(R.id.list_message_main)).setGravity(Gravity.RIGHT);
-            ((LinearLayout)messageRow.findViewById(R.id.userPanel)).setGravity(Gravity.RIGHT);
-        }
-
-        new UserDataAccessRepository(getContext()).getUserByGoogleId(message.getId_user()).observe(activity, new Observer<List<User>>() {
-            @Override
-            public void onChanged(List<User> users) {
-                if (users.size() > 0){
-                    setUserDatas(users.get(0), messageRow);
-                }
-                else{
-                    UserManagement.getUserByGoogleId(message.getId_user()).continueWith(new Continuation<List<User>, Object>() {
-                        @Override
-                        public Object then(@NonNull Task<List<User>> task) throws Exception {
-                            List<User> users = task.getResult();
-                            if(users.size() > 0)
-                                setUserDatas(users.get(0), messageRow);
-                            return null;
-                        }
-                    });
-                }
-            }
-        });
-
-        return messageRow;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View row = layoutInflater.inflate(R.layout.message_list_item, parent, false);
+        return new ViewHolder(row);
     }
 
-    private void setUserDatas(User user, View messageRow){
-        ((TextView)messageRow.findViewById(R.id.emailTxt)).setText(user.getEmail());
-        String avatar = user.getAvatar();
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (messages != null) {
+            Message message = messages.get(position);
+            holder.bind(message);
+        }else{
+        }
+    }
 
-        ImageView imageView = messageRow.findViewById(R.id.userAvatar);
-        if (avatar != null) {
-            Picasso.get().load(avatar).into(imageView);
-        } else {
-            Picasso.get().load("https://i1.pngguru.com/preview/792/29/287/button-ui-2-google-android-robot-icon-png-clipart.jpg").into(imageView);
+    @Override
+    public int getItemCount() {
+        return messages == null ? 0 : messages.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private final View view;
+        private Message message;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            this.view = view;
+        }
+
+        public void bind(Message message){
+            this.message = message;
+            displayDate();
+            displayContent();
+            definePosition();
+            displayUserDatas();
+        }
+
+        private void displayDate(){
+            String date;
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+            String today = formatter.format(new Date());
+            String convDate = formatter.format(message.getDate());
+            if (today.compareTo(convDate) == 0){
+                date = new SimpleDateFormat("hh:mm").format(message.getDate());
+            }
+            else {
+                date = convDate;
+            }
+            ((TextView)view.findViewById(R.id.dateTxt)).setText(date);
+        }
+
+        private void displayContent(){
+            ((TextView)view.findViewById(R.id.messageTxt)).setText(message.getContent());
+        }
+
+        private void definePosition(){
+            if(message.getId_user().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                ((LinearLayout)view.findViewById(R.id.list_message_main)).setGravity(Gravity.RIGHT);
+                ((LinearLayout)view.findViewById(R.id.userPanel)).setGravity(Gravity.RIGHT);
+            }
+        }
+
+        private void displayUserDatas(){
+            new UserDataAccessRepository(layoutInflater.getContext()).getUserByGoogleId(message.getId_user()).observe(activity, new Observer<List<User>>() {
+                @Override
+                public void onChanged(List<User> users) {
+                    if (users.size() > 0){
+                        setUserDatas(users.get(0));
+                    }
+                    else{
+                        UserManagement.getUserByGoogleId(message.getId_user()).continueWith(new Continuation<List<User>, Object>() {
+                            @Override
+                            public Object then(@NonNull Task<List<User>> task) throws Exception {
+                                List<User> users = task.getResult();
+                                if(users.size() > 0)
+                                    setUserDatas(users.get(0));
+                                return null;
+                            }
+                        });
+                    }
+                }
+            });
+        }
+        private void setUserDatas(User user){
+            ((TextView)view.findViewById(R.id.emailTxt)).setText(user.getEmail());
+            String avatar = user.getAvatar();
+
+            ImageView imageView = view.findViewById(R.id.userAvatar);
+            if (avatar != null) {
+                Picasso.get().load(avatar).into(imageView);
+            } else {
+                Picasso.get().load("https://i1.pngguru.com/preview/792/29/287/button-ui-2-google-android-robot-icon-png-clipart.jpg").into(imageView);
+            }
         }
     }
 }
